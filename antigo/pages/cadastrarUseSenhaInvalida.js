b2e41c4b2aba3e77e5b18e4d7b5978c062f6d8dc
jonparo <164812529+jonparo@users.noreply.github.com>
2024-04-05 00:39:55 -0300

    new file:   antigo/pages/adicionarAoCarrinho.js 	new file:   antigo/pages/cadastrarUseEmailInvalido.js 	new file:   antigo/pages/cadastrarUseSenhaInvalida.js 	new file:   antigo/pages/cadastrarUsuario.js 	new file:   antigo/pages/enviarMensagemParaSuporte.js 	new file:   antigo/pages/fazerCompra.js 	new file:   antigo/pages/preenchimentoCadastro.js 	new file:   antigo/pages/utils.js 	new file:   antigo/tests/main.js 	new file:   antigo/tests/package-lock.json 	new file:   antigo/tests/package.json

---
+++ antigo/pages/cadastrarUseSenhaInvalida.js
@@ -0,0 +1,19 @@
+const { By, until } = require('selenium-webdriver');
+const { sleep } = require('./utils');
+const preenchimentoCadastro = require('./preenchimentoCadastro');
+ 
+async function cadastrarUseSenhaInvalida(driver) {
+    await driver.get("https://advantageonlineshopping.com/#/");
+    await sleep(3000);
+    await driver.findElement(By.id("menuUser")).click();
+    await sleep(3000);
+    await driver.wait(until.elementLocated(By.css("a.create-new-account"))).click();
+    await sleep(3000);
+
+    await driver.findElement(By.name("passwordRegisterPage")).sendKeys("senha123");
+    await driver.findElement(By.name("confirm_passwordRegisterPage")).sendKeys("senha123");
+    await driver.findElement(By.name("emailRegisterPage")).sendKeys("exemplo@gmail.com");
+    await preenchimentoCadastro(driver);
+}
+
+module.exports = cadastrarUseSenhaInvalida;
